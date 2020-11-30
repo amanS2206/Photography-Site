b2b369c86aedc67995ffbab28317a741130b79b3
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-12-01 00:33:52 +0530

    add more functionality

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ const showBtn = document.querySelector('.navbtn');
 const topNav = document.querySelector('.top-nav');
 
 showBtn.addEventListener('click', function(){
-    if(topNav.classList.contains('showNav')){
-        topNav.classList.remove('showNav');
+    if(topNav.classList.contains('shownav')){
+        topNav.classList.remove('shownav');
         showBtn.innerHTML = '<i class = "fa fa-bars"></i>';     
     }else {
-        topNav.classList.add('showNav');
+        topNav.classList.add('shownav');
         showBtn.innerHTML = '<i class= "fa fa-times"></i>'
     }
 })
