5425ddb1452fbaa93a131572b7cc717bf69fd805
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-12-01 00:16:40 +0530

    add some functionality

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
 const showBtn = document.querySelector('.navbtn');
 const topNav = document.querySelector('.top-nav');
+
+showBtn.addEventListener('click', function(){
+    if(topNav.classList.contains('showNav')){
+        topNav.classList.remove('shadow');
+        showBtn.innerHTML = '<i class = "fa fa-bars"></i>';     
+    }else {
+        topNav.classList.add('showNav');
+        showBtn.innerHTML = '<i class= "fa fa-times"></i>'
+    }
+})
