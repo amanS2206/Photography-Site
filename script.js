565a8d8da2f979756650f15d512b15c9b52b5535
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-12-01 00:27:01 +0530

    made minor changes

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const topNav = document.querySelector('.top-nav');
 
 showBtn.addEventListener('click', function(){
     if(topNav.classList.contains('showNav')){
-        topNav.classList.remove('shadow');
+        topNav.classList.remove('showNav');
         showBtn.innerHTML = '<i class = "fa fa-bars"></i>';     
     }else {
         topNav.classList.add('showNav');
