1356a0dd4b2ace2a5110e5c0d2b24cb70d020b8f
amanS2206 <aman.saxena_cs18@gla.ac.in>
2020-12-04 19:53:11 +0530

    add auto writing

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const showBtn = document.querySelector('.navbtn');
 const topNav = document.querySelector('.top-nav');
+const write = document.querySelector('.auto');
 
 showBtn.addEventListener('click', function(){
     if(topNav.classList.contains('shownav')){
@@ -10,3 +11,18 @@ showBtn.addEventListener('click', function(){
         showBtn.innerHTML = '<i class= "fa fa-times"></i>';
     }
 })
+
+function writeText(){
+    const type = "Welcome to Photography Site";
+    let i = 0;
+    const typing = () => {
+        if(i < type.length){
+            write.innerHTML += type.charAt(i);
+            i++;
+            setTimeout(typing, 30);
+        }
+    };
+    typing();
+}
+
+writeText();
